79d10e76faffbb1a7438b79433e697486ed1dc9b
Luke-ng111 <lukengtp@gmail.com>
2025-05-07 16:09:52 +0800

    Datatype demo preparation (section 4)
    Section 4 is complete, was intending to submit after finishing section 5 but I want to do other stuff and am very tired so I will complete this another time.

--- Assets/Assets/Worksheets/Week_03/C# Programming/C# Datatypes/Scripts/datatypedemo.cs
+++ Assets/Assets/Worksheets/Week_03/C# Programming/C# Datatypes/Scripts/datatypedemo.cs
@@ -6,9 +6,10 @@ namespace ProjectNamespace2
     public class datatypedemo : MonoBehaviour
     {
         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
-
+            Debug.Log("Data Types Demo");
+            Debug.Log("Hello World");
         }
 
         // Update is called once per frame
