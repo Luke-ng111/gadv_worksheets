51c1f1ee71745441ace8d643f1bb660ae02656a9
Luke-ng111 <lukengtp@gmail.com>
2025-05-22 17:24:28 +0800

    Continuing work on OOP basics
    Not complete with the worksheet

--- Assets/Assets/Worksheets/Week_03/C# OOP/Scripts/player.cs
+++ Assets/Assets/Worksheets/Week_03/C# OOP/Scripts/player.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    
+    public class Player
+    {
+        private int health = 1000;
+
+        
+
+        public int GetHealth(int amount)
+        {
+            return health;
+        }
+        public void TakeDamage(int amount)
+        {
+            Debug.Log("damage taken");
+            health -= amount;
+            
+        }
+    }
 }
