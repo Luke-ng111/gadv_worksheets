c48a1395ce42630319238120a6af6be914f97781
Luke-ng111 <lukengtp@gmail.com>
2025-05-22 16:49:25 +0800

    Exercise 1 for Basic Scripting complete
    Skipping the remainder of OOP Basics first because I am stupid, I completed Exercise 1 for Basic Scripting as I had hoped to jump forward to Monobehaviour Programming first. I will complete said worksheet after I am done.

---
+++ Assets/Assets/Worksheets/Week_03/Basic scripting/scripts/gamemanager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("This script is attached to a GameObject!");
+    }
+
+
+
+// Update is called once per frame
+    void Update()
+        {
+        
+        }
+    }
